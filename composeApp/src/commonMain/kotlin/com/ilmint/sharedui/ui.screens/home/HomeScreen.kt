e1d23817f249f6c36c839ab546b422472640d3bc
Iliuminate <carlosdz.centi@gmail.com>
2024-08-20 20:03:41 -0500

    Se agrega modulo de ViewModel y un loeader ficticio

--- composeApp/src/commonMain/kotlin/com/ilmint/sharedui/ui.screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ilmint/sharedui/ui.screens/home/HomeScreen.kt
@@ -2,14 +2,17 @@ package com.ilmint.sharedui.ui.screens.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -17,11 +20,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 
 import com.ilmint.sharedui.Movie
@@ -34,7 +39,10 @@ import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(onMovieClick: (Movie) -> Unit) {
+fun HomeScreen(
+    onMovieClick: (Movie) -> Unit,
+    vm: HomeViewModel = viewModel{ HomeViewModel() } // Esto sirve para que reutilicemos el viewmodel que creamos anteriormente y no se recomponga todas las veces
+) {
     Screen {
         val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
         Scaffold(
@@ -46,6 +54,17 @@ fun HomeScreen(onMovieClick: (Movie) -> Unit) {
             },
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
         ) { padding ->
+
+            val state = vm.state
+            if(state.isLoading){
+                Box(
+                    modifier = Modifier.fillMaxSize().padding(padding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
             LazyVerticalGrid(
                 columns = GridCells.Adaptive(120.dp),
                 contentPadding = PaddingValues(4.dp),
@@ -53,7 +72,7 @@ fun HomeScreen(onMovieClick: (Movie) -> Unit) {
                 verticalArrangement = Arrangement.spacedBy(4.dp),
                 modifier = Modifier.padding(padding)
             ) {
-                items(movies, key = { it.id }) {
+                items(state.movies, key = { it.id }) {
                     MovieItem(movie = it, onClick = { onMovieClick(it) })
                 }
             }
